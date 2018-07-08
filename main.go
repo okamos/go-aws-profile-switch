0604ab7cebc1cddb4795e30071837261f5108e3e
okamos <himinato.k@gmail.com>
2018-07-08 18:10:57 +0900

    Add sw command

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"log"
@@ -16,7 +17,12 @@ import (
 const (
 	keyName    = "aws_access_key_id"
 	secretName = "aws_secret_access_key"
+	regionName = "region"
+	outputName = "output"
+	swUsage    = "Usage: awsswitch ws -p [profile name]"
 )
+
+var credentialsPath string
 
 // LsCommand defines list AWS profiles
 type LsCommand struct{}
@@ -33,12 +39,193 @@ func (c *LsCommand) Help() string {
 
 // Run ls command
 func (c *LsCommand) Run(args []string) int {
+	credentials, _ := LoadCredentials()
+
+	var defaultKey string
+	for _, v := range credentials {
+		if v["profile"] == "default" {
+			defaultKey = v[keyName]
+			break
+		}
+	}
+	fmt.Println("Available profiles")
+	for _, v := range credentials {
+		if v["profile"] != "default" && v[keyName] == defaultKey {
+			fmt.Println("* " + v["profile"])
+		} else {
+			fmt.Println("  " + v["profile"])
+		}
+	}
+
+	return 0
+}
+
+// SwCommand defines switch default aws profile
+type SwCommand struct{}
+
+// Synopsis about sw
+func (c *SwCommand) Synopsis() string {
+	return "Switches default your AWS profile"
+}
+
+// Help about sw
+func (c *SwCommand) Help() string {
+	return swUsage
+}
+
+// Run sw command
+func (c *SwCommand) Run(args []string) int {
+	var profile string
+
+	flags := flag.NewFlagSet("sw", flag.ContinueOnError)
+	flags.Usage = func() { fmt.Println(swUsage) }
+	flags.StringVar(&profile, "profile", "", "A profile name from your credentials")
+	flags.StringVar(&profile, "p", "", "A profile name from your credentials")
+
+	if err := flags.Parse(args); err != nil {
+		return 1
+	}
+	if profile == "" {
+		fmt.Println(swUsage)
+		return 1
+	}
+
+	credentials, comments := LoadCredentials()
+
+	var defaultKey string
+	candidate := map[string]string{}
+	for _, v := range credentials {
+		if v["profile"] == "default" {
+			defaultKey = v[keyName]
+		} else if v["profile"] == profile {
+			candidate["profile"] = profile
+			candidate[keyName] = v[keyName]
+			candidate[secretName] = v[secretName]
+			candidate[regionName] = v[regionName]
+			candidate[outputName] = v[outputName]
+		}
+	}
+	if candidate["profile"] == "" {
+		fmt.Printf("The profile %s is not found\n", profile)
+		return 1
+	}
+	if defaultKey == candidate[keyName] {
+		fmt.Printf("Your default profile set %s already\n", profile)
+		return 0
+	}
+
+	isBackup := defaultKey == ""
+	for _, v := range credentials {
+		if v[keyName] == defaultKey {
+			isBackup = true
+			break
+		}
+	}
+	if !isBackup {
+		s := "Your default profile will be overwrite `" + profile + "`\n"
+		fmt.Printf("%s You should be backup default profile.", s)
+		return 1
+	}
+
+	var buf []byte
+	buf = append(buf, ("[default]\n")...)
+	fmt.Println("Your default profile is overwrote")
+	if candidate[keyName] != "" {
+		buf = append(buf, (keyName + "=" + candidate[keyName] + "\n")...)
+		fmt.Println("> " + keyName + "=" + candidate[keyName])
+	}
+	if candidate[secretName] != "" {
+		buf = append(buf, (secretName + "=" + candidate[secretName] + "\n")...)
+		fmt.Println("> " + secretName + "=" + candidate[secretName])
+	}
+	if candidate[regionName] != "" {
+		buf = append(buf, (regionName + "=" + candidate[regionName] + "\n")...)
+		fmt.Println("> " + regionName + "=" + candidate[regionName])
+	}
+	if candidate[outputName] != "" {
+		buf = append(buf, (outputName + "=" + candidate[outputName] + "\n")...)
+		fmt.Println("> " + outputName + "=" + candidate[outputName])
+	}
+	buf = append(buf, "\n"...)
+
+	for _, v := range credentials {
+		if v["profile"] == "default" {
+			fmt.Print("\nFrom\n")
+			if v[keyName] != "" {
+				fmt.Println("> " + keyName + "=" + v[keyName])
+			}
+			if v[secretName] != "" {
+				fmt.Println("> " + secretName + "=" + v[secretName])
+			}
+			if v[regionName] != "" {
+				fmt.Println("> " + regionName + "=" + v[regionName])
+			}
+			if v[outputName] != "" {
+				fmt.Println("> " + outputName + "=" + v[outputName])
+			}
+			continue
+		}
+		buf = append(buf, ("[" + v["profile"] + "]\n")...)
+		if v[keyName] != "" {
+			buf = append(buf, (keyName + "=" + v[keyName] + "\n")...)
+		}
+		if v[secretName] != "" {
+			buf = append(buf, (secretName + "=" + v[secretName] + "\n")...)
+		}
+		if v[regionName] != "" {
+			buf = append(buf, (regionName + "=" + v[regionName] + "\n")...)
+		}
+		if v[outputName] != "" {
+			buf = append(buf, (outputName + "=" + v[outputName] + "\n")...)
+		}
+		buf = append(buf, "\n"...)
+	}
+	buf = append(buf, comments...)
+
+	file, err := os.OpenFile(credentialsPath, os.O_RDWR|os.O_TRUNC, 0666)
+	if err != nil {
+		fmt.Println(err)
+		return 1
+	}
+	defer file.Close()
+	file.Write(buf)
+
+	return 0
+}
+
+func main() {
+	u, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	credentialsPath = filepath.Join(u.HomeDir, ".aws", "credentials")
+	c := cli.NewCLI("awsswitch", "0.1.0")
+	c.Args = os.Args[1:]
+	c.Commands = map[string]cli.CommandFactory{
+		"ls": func() (cli.Command, error) {
+			return &LsCommand{}, nil
+		},
+		"sw": func() (cli.Command, error) {
+			return &SwCommand{}, nil
+		},
+	}
+
+	exitCode, err := c.Run()
+	if err != nil {
+		fmt.Printf("Failed to execute: %s\n", err.Error())
+	}
+	os.Exit(exitCode)
+}
+
+// LoadCredentials parses AWS credentials file
+func LoadCredentials() ([]map[string]string, []byte) {
 	fp := getFile()
 	defer fp.Close()
 
 	reader := bufio.NewReaderSize(fp, 256)
-
 	credentials := []map[string]string{}
+	var comments []byte
 	current := -1
 	for {
 		bufLine, _, err := reader.ReadLine()
@@ -57,58 +244,28 @@ func (c *LsCommand) Run(args []string) int {
 				"profile": strings.Trim(line[1:len(line)-1], " "),
 			})
 			current++
-		} else {
+		} else if line[0] == byte('#') {
+			comments = append(comments, (line + "\n")...)
+		} else if strings.Contains(line, "=") {
 			pair := strings.Split(line, "=")
-			if strings.Trim(pair[0], " ") == keyName {
-				credentials[current][keyName] = strings.Trim(pair[1], " ")
-			} else if strings.Trim(pair[0], " ") == secretName {
-				credentials[current][secretName] = strings.Trim(pair[1], " ")
+			val := strings.Trim(pair[1], " ")
+			switch strings.Trim(pair[0], " ") {
+			case keyName:
+				credentials[current][keyName] = val
+			case secretName:
+				credentials[current][secretName] = val
+			case regionName:
+				credentials[current][regionName] = val
+			case outputName:
+				credentials[current][outputName] = val
 			}
 		}
 	}
-
-	var defaultKey string
-	for _, v := range credentials {
-		if v["profile"] == "default" {
-			defaultKey = v[keyName]
-		}
-	}
-	fmt.Println("Available profiles")
-	for _, v := range credentials {
-		if v["profile"] != "default" && v[keyName] == defaultKey {
-			fmt.Println("* " + v["profile"])
-		} else {
-			fmt.Println("  " + v["profile"])
-		}
-	}
-
-	return 0
-}
-
-func main() {
-	c := cli.NewCLI("awsswitch", "0.1.0")
-	c.Args = os.Args[1:]
-	c.Commands = map[string]cli.CommandFactory{
-		"ls": func() (cli.Command, error) {
-			return &LsCommand{}, nil
-		},
-	}
-
-	exitCode, err := c.Run()
-	if err != nil {
-		fmt.Printf("Failed to execute: %s\n", err.Error())
-	}
-	os.Exit(exitCode)
+	return credentials, comments
 }
 
 func getFile() *os.File {
-	u, err := user.Current()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	credentials := filepath.Join(u.HomeDir, ".aws", "credentials")
-	fp, err := os.Open(credentials)
+	fp, err := os.Open(credentialsPath)
 	if err != nil {
 		log.Fatal(err)
 	}
